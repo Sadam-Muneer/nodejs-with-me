219876f587cc62298d25250f385604abbe5c4251
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-06-20 17:59:04 +0500

    modules and lodash package usage

---
+++ backend/notes.js
@@ -0,0 +1,11 @@
+console.log("Notes page is loaded");
+const age = 24;
+
+const addnumber = function (a, b) {
+  return a + b;
+};
+
+module.exports = {
+  age,
+  addnumber,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const os = require("os");
+var _ = require("lodash");
+const Notes = require("./notes");
 const user = os.userInfo();
+var age = Notes.age;
+var result = Notes.addnumber(age + 18, 10);
+console.log(result);
+console.log(age);
 console.log(user);
 fs.appendFile(
   "greeting.txt",
@@ -9,3 +15,7 @@ fs.appendFile(
     console.log("file is created");
   }
 );
+const uniquedata = ["person", "person", 1, 2, 1, 2, "name", "age", "age"];
+var filter = _.uniq(uniquedata);
+console.log(filter);
+console.log(_.isString("sadam"));
