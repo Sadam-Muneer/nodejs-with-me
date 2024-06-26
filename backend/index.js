73ec5fbeb78bd66ae5670dbfcf3aed7e1a2b4ab4
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-06-26 15:58:14 +0500

    middlewares added in previous code

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,14 @@ const db = require("./db");
 const PersonRoutes = require("./Routes/person.routes");
 const MenuRoutes = require("./Routes/menu.routes");
 require("dotenv").config();
+// Middleware function
+const logRequest = (req, res, next) => {
+  console.log(
+    `${new Date().toLocaleString()} Request Made to: ${req.originalUrl}`
+  );
+  next();
+};
+app.use(logRequest);
 
 app.use(bodyParser.json());
 app.use("/person", PersonRoutes);
